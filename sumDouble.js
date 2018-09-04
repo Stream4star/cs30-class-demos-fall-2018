0dfeef1d6d63ee2bbc599e148f689ce6a18aba84
Dan Schellenberg <schellenberg@gmail.com>
2018-09-04 15:33:18 -0600

    first day demos

---
+++ sumDouble.js
@@ -0,0 +1,9 @@
+function sum_double(a, b) {
+  let theSum = a + b;
+  if (a == b) {
+    return the_sum * 2;
+  }
+  else {
+    return the_sum;
+  }
+}
---
+++ sum_double.py
@@ -0,0 +1,6 @@
+def sum_double(a, b):
+    the_sum = a + b
+    if a == b:
+        return the_sum * 2
+    else:
+        return the_sum
