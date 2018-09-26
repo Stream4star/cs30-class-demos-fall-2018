83b22451e6ac3261443eb5d019eb9c629624ac6d
Dan Schellenberg <schellenberg@gmail.com>
2018-09-26 13:50:46 -0600

    finished version

--- trafficlight/sketch.js
+++ trafficlight/sketch.js
@@ -6,13 +6,67 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/
 
+let state;
+let lastTimeSwitchedColor;
+
+const RED_LIGHT_DURATION = 3000;
+const GREEN_LIGHT_DURATION = 4000;
+const YELLOW_LIGHT_DURATION = 1000;
+
 function setup() {
   createCanvas(600, 600);
+  state = 1;
+  lastTimeSwitchedColor = 0;
 }
 
 function draw() {
   background(255);
   drawOutlineOfLights();
+  checkForStateChange();
+  displayCorrectLight();
+}
+
+function checkForStateChange() {
+  let elapsedTime = millis() - lastTimeSwitchedColor;
+  if (state === 1 && elapsedTime >= RED_LIGHT_DURATION) {
+    state = 2;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 2 && elapsedTime >= GREEN_LIGHT_DURATION) {
+    state = 3;
+    lastTimeSwitchedColor = millis();
+  }
+  else if (state === 3 && elapsedTime >= YELLOW_LIGHT_DURATION) {
+    state = 1;
+    lastTimeSwitchedColor = millis();
+  }
+}
+
+function displayCorrectLight() {
+  if (state === 1) {
+    displayRedLight();
+  }
+  else if (state === 2) {
+    displayGreenLight();
+  }
+  else if (state === 3) {
+    displayYellowLight();
+  }
+}
+
+function displayRedLight() {
+  fill(255, 0, 0);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
+}
+
+function displayYellowLight() {
+  fill(255, 255, 0);
+  ellipse(width/2, height/2, 50, 50); //middle
+}
+
+function displayGreenLight() {
+  fill(0, 255, 0);
+  ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }
 
 function drawOutlineOfLights() {
