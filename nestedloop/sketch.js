dd3a8afe29010361460ee2c590155b8f563b6f2e
Dan Schellenberg <schellenberg@gmail.com>
2018-10-02 13:47:54 -0600

    chessboard

---
+++ nestedloop/sketch.js
@@ -0,0 +1,25 @@
+let boxSize;
+let isFilledWithBlack;
+
+function setup() {
+  createCanvas(600, 600);
+  boxSize = width/8;
+  isFilledWithBlack = false;
+}
+
+function draw() {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (isFilledWithBlack) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(i * boxSize, j * boxSize, boxSize, boxSize);
+      isFilledWithBlack = !isFilledWithBlack;
+    }
+    isFilledWithBlack = !isFilledWithBlack;
+  }
+
+}
