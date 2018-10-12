ff00e52f8b1ee94d2fc10ccf4cdda2626b8786d1
Dan Schellenberg <schellenberg@gmail.com>
2018-10-12 13:23:03 -0600

    array demo

---
+++ multipleballobjects/sketch.js
@@ -0,0 +1,33 @@
+let ballArray = [];
+
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+  for (let i=0; i<ballArray.length; i++) {
+    fill(ballArray[i].fillColor);
+    ellipse(ballArray[i].x, ballArray[i].y, ballArray[i].radius*2, ballArray[i].radius*2);
+  }
+}
+
+function mousePressed() {
+  let ball = {
+    x: mouseX,
+    y: mouseY,
+    radius: 25,
+    fillColor: color(255, 0, 0),
+  };
+  ballArray.push(ball);
+}
+
+function keyTyped() {
+  if (key === "w") {
+    ball.radius *= 1.1;
+  }
+  else if (key === "s") {
+    ball.radius *= 0.9;
+  }
+}
