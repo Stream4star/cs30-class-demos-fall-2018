f4807b65d46f00c1b80679813245950e724e8cd8
Dan Schellenberg <schellenberg@gmail.com>
2018-10-24 13:33:45 -0600

    current grid thing

--- p5/sketch.js
+++ p5/sketch.js
@@ -8,13 +8,28 @@ let grid;
 let cellSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 600);
   cellSize = width / cols;
   grid = createRandom2dArray(cols, rows);
 }
 
 function draw() {
-  
+  background(255);
+  displayGrid();
+}
+
+function displayGrid() {
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      if (grid[y][x] === 0) {
+        fill(0);
+      }
+      else {
+        fill(255);
+      }
+      rect(x*cellSize, y*cellSize, cellSize, cellSize);
+    }
+  }
 }
 
 function createRandom2dArray(cols, rows) {
