db2d448193d8e960910f05bf5a73d0bc64fe510c
Dan Schellenberg <schellenberg@gmail.com>
2018-12-05 15:20:56 -0600

    updated version

--- dodge/sketch.js
+++ dodge/sketch.js
@@ -8,12 +8,19 @@ let cols = 4;
 let grid;
 let cellSize;
 let carImg, carX, carY;
-let leftEdgeOfGrid;
+let truckImg;
+let leftEdgeOfGrid, rightEdgeOfGrid;
+let speed = 20;
+let isAlive = true;
+let score = 0;
+
 const EMPTY_CELL = 0;
 const CAR_CELL = 1;
+const TRUCK_CELL = 2;
 
 function preload() {
-  carImg = loadImage("assets/car.png");
+  carImg = loadImage("assets/car-truck1.png");
+  truckImg = loadImage("assets/car-truck2.png");
 }
 
 
@@ -22,19 +29,23 @@ function setup() {
   grid = createEmpty2dArray(cols, rows);
   cellSize = height / rows;
   leftEdgeOfGrid = width/2 - (cols*cellSize)/2;
+  rightEdgeOfGrid = leftEdgeOfGrid + (cols*cellSize);
   
   //place car
   carY = rows - 1;
   carX = 0;
   grid[carY][carX] = CAR_CELL;
-  
+
   setupSwipeHandling();
 }
 
-
 function draw() {
-  background(255);
-  displayGrid(grid, leftEdgeOfGrid);
+  if (isAlive) {
+    gamePlay();
+  }
+  else {
+    deathScreen();
+  }
 }
 
 function keyPressed() {
@@ -46,9 +57,60 @@ function keyPressed() {
   }
 }
 
+function gamePlay() {
+  background(255);
+  displayLanes();
+  if (frameCount % speed === 0) {
+    updateGrid();
+    spawnEnemy();
+  }
+  displayGrid(grid, leftEdgeOfGrid);
+}
+
+function deathScreen() {
+  background(255);
+  fill(255, 0, 0);
+  textSize(42);
+  textAlign(CENTER, CENTER);
+  text("You are dead!\nYou dodged " + score + " vehicles.", width/2, height/2);
+
+}
+
+function updateScoreAndSpeed() {
+  score += 1;
+  if (score > 10) {
+    speed = 15;
+  }
+  else if (score > 20) {
+    speed = 10;
+  } 
+  else if (score > 35) {
+    speed = 8;
+  } 
+  else if (score > 50) {
+    speed = 5;
+  }
+  else if (score > 100) {
+    speed = 3;
+  }
+  else if (score > 200) {
+    speed = 1;
+  }
+}
+
+function spawnEnemy() {
+  if (random(100) > 50) {
+    let whichLane = Math.floor(random(cols));
+    grid[0][whichLane] = TRUCK_CELL;
+  }
+}
+
 function moveLeft() {
   if (carX > 0) {
     //check for collision
+    if (grid[carY][carX-1] !== EMPTY_CELL) {
+      isAlive = false;
+    }
 
     //if no collision
     grid[carY][carX] = EMPTY_CELL;
@@ -60,6 +122,9 @@ function moveLeft() {
 function moveRight() {
   if (carX < cols - 1) {
     //check for collision
+    if (grid[carY][carX+1] !== EMPTY_CELL) {
+      isAlive = false;
+    }
 
     //if no collision
     grid[carY][carX] = EMPTY_CELL;
@@ -86,10 +151,10 @@ function setupSwipeHandling() {
 
 function swiped(event) {
   if (event.direction === Hammer.DIRECTION_RIGHT) {
-    moveLeft();
+    moveRight();
   }
   else if (event.direction === Hammer.DIRECTION_LEFT) {
-    moveRight();
+    moveLeft();
   }
   else if (event.direction === Hammer.DIRECTION_UP) {
     console.log("you swiped up");
@@ -102,13 +167,38 @@ function swiped(event) {
 function displayGrid(grid, xOffset = 0) {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === 0) {
-        fill(255);
+      if (grid[y][x] === CAR_CELL) {
+        image(carImg, x*cellSize + xOffset + cellSize/4, y*cellSize, cellSize/2, cellSize);
       }
-      else {
-        fill(0);
+      else if (grid[y][x] === TRUCK_CELL) {
+        image(truckImg, x*cellSize + xOffset + cellSize/4, y*cellSize, cellSize/2, cellSize);
       }
-      rect(x*cellSize + xOffset, y*cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function updateGrid() {
+  for (let y = rows - 1; y >= 0; y--) {
+    for (let x = cols - 1; x >= 0; x--) {
+      if (grid[y][x] === TRUCK_CELL) {
+        if (y + 1 < rows) {
+          //vehicle is still going to be on the grid
+          if (grid[y+1][x] === CAR_CELL) {
+            //ran into the car
+            isAlive = false;
+          }
+          else {
+            //vehicle is moving into an open space
+            grid[y+1][x] = TRUCK_CELL;
+            grid[y][x] = EMPTY_CELL;
+          }
+        }
+        else {
+          //vehicle is dropping off the bottom of the grid
+          grid[y][x] = EMPTY_CELL;
+          updateScoreAndSpeed();
+        }
+      }
     }
   }
 }
@@ -122,4 +212,20 @@ function createEmpty2dArray(cols, rows) {
     }
   }
   return emptyGrid;
+}
+
+function displayLanes() {
+  //create a black rect to hold the whole road
+  fill(0);
+  rect(leftEdgeOfGrid, 0, rightEdgeOfGrid - leftEdgeOfGrid, height);
+
+  //add lanes
+  stroke(255);
+  strokeWeight(2);
+  for (let x = 0; x < 4; x++) {
+    let laneX = leftEdgeOfGrid + x*(cols * cellSize / 4);
+    for (let i = 0; i < rows; i += 2) {
+      line(laneX, i*cellSize, laneX, (i+1)*cellSize);
+    }
+  }
 }
